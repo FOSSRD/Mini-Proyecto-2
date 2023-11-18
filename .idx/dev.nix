898f1c6253c0856eb263d95e9fc54e68eb93e2f5
josuer08 <josuer08@gmail.com>
2023-11-18 04:23:06 +0000

    Basic structure for the program, not yes decided.

---
+++ .idx/dev.nix
@@ -0,0 +1,24 @@
+{pkgs}: {
+  #channel = "stable-23.05"; # "stable-23.05" or "unstable"
+  packages = [
+    pkgs.go
+    pkgs.gopls
+  ];
+  idx.extensions = [
+    "golang.Go"
+    "r3inbowari.gomodexplorer"
+    "pinage404.bash-extension-pack"
+    "premparihar.gotestexplorer"
+    "jeff-hykim.better-go-syntax"
+    ];
+  #idx.previews = {
+   # enable = true;
+    #previews = [
+     # {
+      #  command = ["go","run","vscpweb.go", "--port", "$PORT"]
+       # manager = "web";
+       # id = "web";
+      #}
+    #];
+  #};
+}
